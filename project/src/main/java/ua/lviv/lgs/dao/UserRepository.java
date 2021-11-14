fe2c312858a36b0542944dda2298bb48670493f1
Stacy Kucherenko <anastasia280919@gmail.com>
2021-11-14 11:18:36 +0200

    Login/Registration form are added

--- project/src/main/java/ua/lviv/lgs/dao/UserRepository.java
+++ project/src/main/java/ua/lviv/lgs/dao/UserRepository.java
@@ -5,5 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import ua.lviv.lgs.domain.User;
 
 public interface UserRepository extends JpaRepository<User, Integer>{
-
+	User findByEmail(String email);
+	
+	User findByActivationCode(String code);
+	
 }
--- project/src/main/java/ua/lviv/lgs/domain/User.java
+++ project/src/main/java/ua/lviv/lgs/domain/User.java
@@ -1,5 +1,6 @@
 package ua.lviv.lgs.domain;
 
+import java.util.Collection;
 import java.util.Set;
 
 import javax.persistence.CollectionTable;
@@ -16,25 +17,40 @@ import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 import javax.persistence.JoinColumn;
 import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+import org.hibernate.validator.constraints.Length;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @Entity
 @Table(name = "user")
 @Inheritance(strategy = InheritanceType.JOINED)
-public class User {
+public class User implements UserDetails {
+	private static final long serialVersionUID = 1L;
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "user_id")
 	private Integer id;
 	@Column
+	@NotBlank(message = "Имя пользователя не может быть пустым!")
 	private String firstName;
 	@Column
+	@NotBlank(message = "Фамилия пользователя не может быть пустым!")
 	private String lastName;
 	@Column
+	@NotBlank(message = "Email пользователя не может быть пустым!")
+	@Email(message = "Email пользователя введён некорректно!")
 	private String email;
 	@Column
+	@Length(min = 6, message = "Пароль пользователя должен быть не менее 6 символов!")
 	private String password;
 	@Column
 	private boolean active;
+	@Column
+	private String activationCode;
 
 	@ElementCollection(targetClass = AccessLevel.class, fetch = FetchType.EAGER)
 	@CollectionTable(name = "access_level", joinColumns = @JoinColumn(name = "user_id"))
@@ -100,6 +116,14 @@ public class User {
 
 	public void setActive(boolean active) {
 		this.active = active;
+	}
+	
+	public String getActivationCode() {
+		return activationCode;
+	}
+
+	public void setActivationCode(String activationCode) {
+		this.activationCode = activationCode;
 	}
 
 	public Set<AccessLevel> getAccessLevels() {
@@ -172,5 +196,35 @@ public class User {
 	public String toString() {
 		return "User [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
 				+ ", password=" + password + ", active=" + active + ", accessLevels=" + accessLevels + "]";
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return getAccessLevels();
+	}
+
+	@Override
+	public String getUsername() {
+		return email;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return isActive();
 	}
 }
